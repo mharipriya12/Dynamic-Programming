acdc6797580f464c5edcf2e55b5274e25703a229
mharipriya12 <haripriyamuthukrishnansvg345@gmail.com>
2026-09-23 12:47:10 +0530

    Add LCS algorithm in lcs.cpp
    Implement longest common subsequence algorithm using dynamic programming.

---
+++ Problems/lcs.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string a, b;
+
+    cin >> a >> b;
+
+    int n = a.length();
+    int m = b.length();
+
+    int dp[101][101] = {0};
+
+    for (int i = 1; i <= n; i++) {
+
+        for (int j = 1; j <= m; j++) {
+
+            if (a[i - 1] == b[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(
+                    dp[i - 1][j],
+                    dp[i][j - 1]
+                );
+            }
+        }
+    }
+
+    cout << dp[n][m];
+
+    return 0;
+}
